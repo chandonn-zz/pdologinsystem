76fdab224ab89a3b0f8d9d4f9272f8cc37380e81
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-12-05 18:45:24 -0200

    Fixed code standards

--- login-settings.php
+++ login-settings.php
@@ -1,20 +1,20 @@
 <?php
 
-if ( $user->is_logged_in() ) {
-	$use->redirect( 'home.php' );
-}
-
-if ( isset( $_POST['login_button'] ) ) {
-	$name = $_POST['user_credential'];
-	$pass = $_POST['user_pass'];
-
-	if ( $user->login( $credential, $pass ) ) {
-		$user->redirect( 'home.php' );
-	} else {
-		$message = 'Wrong user or password, try again';
-		unset( $_POST['login_button'] );
-		unset ( $_POST['user_credential'] );
-		unset ( $_POST['user_pass'] );
+	if ( $user->is_logged_in() ) {
+		$use->redirect( 'home.php' );
 	}
-}
+
+	if ( isset( $_POST['login_button'] ) ) {
+		$name = $_POST['user_credential'];
+		$pass = $_POST['user_pass'];
+
+		if ( $user->login( $credential, $pass ) ) {
+			$user->redirect( 'home.php' );
+		} else {
+			$message = 'Wrong user or password, try again';
+			unset( $_POST['login_button'] );
+			unset ( $_POST['user_credential'] );
+			unset ( $_POST['user_pass'] );
+		}
+	}
 ?>
