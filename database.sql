e81cec855c9d5a83ee5582b0050263c93765b6e3
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-12-05 10:25:02 -0200

    Fixed mysql syntax

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE 'loginsystem';
-CREATE TABLE 'loginsystem'.'users' (
-	'id' INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	'name' VARCHAR( 50 ) NOT NULL,
-	'email' VARCHAR( 255 ) NOT NULL,
-	'pass' VARCHAR( 60 ) NOT NULL,
-	UNIQUE( 'name' ),
-	UNIQUE( 'email' )
+CREATE DATABASE IF NOT EXISTS loginsystem;
+CREATE TABLE loginsystem.users (
+	id INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR( 50 ) NOT NULL,
+	email VARCHAR( 255 ) NOT NULL,
+	pass VARCHAR( 60 ) NOT NULL,
+	UNIQUE( name ),
+	UNIQUE( email )
 ) ENGINE = MYISAM;
