ef15dfd030525e5c3f773480a8d49fb503fef0f5
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-12-05 10:42:14 -0200

    Created the database connection file

---
+++ database-connection.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+$host = 'localhost';
+$user = 'root';
+$pass = '1234';
+$db_name = 'loginsystem';
+
+try {
+	$connection = new PDO( 'mysql:host=localhost; dbname=loginsystem', $user, $pass );
+	$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION )
+} catch( PDOException $e ) {
+	echo $e->getMessage();
+}
+
+include_once 'class.user.php';
+$user = new User( $connection );
